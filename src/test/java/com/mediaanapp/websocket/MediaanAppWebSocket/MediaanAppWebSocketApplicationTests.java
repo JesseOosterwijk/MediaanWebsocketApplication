fb669cedc1d0950173bfc05ee6e63771951ce443
TurtlSandwich <i398785@fhict.nl>
2020-03-11 17:13:14 +0100

    First Commit

---
+++ src/test/java/com/mediaanapp/websocket/MediaanAppWebSocket/MediaanAppWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediaanapp.websocket.MediaanAppWebSocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaanAppWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
